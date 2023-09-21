5cd810dc5553aa66ca87aa260fa07d2fd1867449
YannickSCF <yannickscf@gmail.com>
2023-09-21 12:17:34 +0200

    First Commit

---
+++ Assets/Runtime/LSTournamentEnums.cs
@@ -0,0 +1,6 @@
+namespace YannickSCF.LSTournaments.Common {
+
+    public enum PouleAssignType { OneByOne, PouleByPoule, Custom }
+
+    public enum ParticipantSelectionType { Random, ByLevel, ByRank }
+}
